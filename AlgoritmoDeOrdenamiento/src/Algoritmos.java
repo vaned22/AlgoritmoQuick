fc4bff5a85c36e5114a59cb75ce5f551aa354dcf
vanessa <31457282+vaned22@users.noreply.github.com>
2017-11-30 22:12:01 -0600

    Add files via upload

---
+++ AlgoritmoDeOrdenamiento/src/Algoritmos.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author vanessa albarran
+ */
+public class Algoritmos {
+    public static void main(String[] args) {
+        AlgoritmosDeOrdenamiento ordenar = new AlgoritmosDeOrdenamiento();
+        int vector1[] = {5, 6, 3, 44, 22, 1};
+        int vector2[] = {55, 4, 43, 44, 2, 10};
+        int vector3[] = {5, 2,1,8,3,9,7};
+
+        /*System.out.println("Arreglo original");
+        ordenar.mostrarArreglo(vector1);
+        System.out.println("Arreglo ordenado con burbuja 1");
+        ordenar.burbuja1(vector1);
+        ordenar.mostrarArreglo(vector1);
+        System.out.println();
+        System.out.println("Arreglo original");
+        ordenar.mostrarArreglo(vector2);
+        System.out.println("Arreglo ordenado con burbuja 2");
+        ordenar.burbuja1(vector2);
+        ordenar.mostrarArreglo(vector2);*/
+
+        System.out.println("Arreglo orginal");
+        //ordenar.mostrarArreglo(vector2);
+        //ordenar.radix(vector2);
+        ordenar.mostrarArreglo(vector3);
+        System.out.println("Arreglo Ordenado con Quick");
+        ordenar.quick(vector3, 0, vector3.length-1);
+
+    }
+}
+
